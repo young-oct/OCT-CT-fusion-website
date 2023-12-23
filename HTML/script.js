f84e96fd0850eaae4ebf758db2c58b1fa24e79e3
young-oct <>
2023-12-23 15:49:03 -0400

    update: v4 home page fixed the text scaling issue wiht figure tag instead of using image tag

--- HTML/script.js
+++ HTML/script.js
@@ -1,26 +1,21 @@
-document.addEventListener('DOMContentLoaded', function(){
-    var menuBar = document.getElementById('menu-bar');
-    var navbarlinks = document.getElementById('nav-links');
-    var body = document.body;
+document.addEventListener("DOMContentLoaded", function () {
+  var menuBar = document.getElementById("menu-bar");
+  var navbarlinks = document.getElementById("nav-links");
 
+  menuBar.addEventListener("click", function () {
+    if (navbarlinks.classList.toggle("active")) {
+      document.body.style.overflowY = "hidden";
+    } else {
+      document.body.style.overflowY = "auto";
+    }
+  });
 
-    menuBar.addEventListener('click',function(){
-        if (navbarlinks.classList.toggle('active')){
-        document.body.style.overflowY = 'hidden'
-        } else {
-        document.body.style.overflowY = 'auto';
-        }
+  window.addEventListener("resize", function () {
+    // Check if the screen width is more than 600px
 
-    });
-
-    window.addEventListener('resize',function(){
-                // Check if the screen width is more than 600px
-
-        if (window.innerWidth > 600){
-            // If so, ensure the nav-links are not in the 'active' state
-            navbarlinks.classList.remove('active')
-
-        }
-    });
-
+    if (window.innerWidth > 600) {
+      // If so, ensure the nav-links are not in the 'active' state
+      navbarlinks.classList.remove("active");
+    }
+  });
 });
