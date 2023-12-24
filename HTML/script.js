67c9ba216025a0eebeecb8f76b1bd9f032d802b1
young-oct <>
2023-12-24 01:18:10 -0400

    update: included a responsive navbar and subnav bar

--- HTML/script.js
+++ HTML/script.js
@@ -1,21 +1,56 @@
 document.addEventListener("DOMContentLoaded", function () {
-  var menuBar = document.getElementById("menu-bar");
-  var navbarlinks = document.getElementById("nav-links");
+  // Select the dropdown button
+  var dropdownBtn = document.querySelector(".subnav_dropbtn");
 
-  menuBar.addEventListener("click", function () {
-    if (navbarlinks.classList.toggle("active")) {
-      document.body.style.overflowY = "hidden";
-    } else {
-      document.body.style.overflowY = "auto";
-    }
-  });
+  // Check if dropdownBtn exists to avoid null reference errors
+  if (dropdownBtn) {
+    // Toggle dropdown on click
+    dropdownBtn.addEventListener("click", function (event) {
+      // Prevent the default anchor behavior
+      event.preventDefault();
 
-  window.addEventListener("resize", function () {
-    // Check if the screen width is more than 600px
+      // Get the dropdown content associated with this button
+      var dropdownContent = this.nextElementSibling;
 
-    if (window.innerWidth > 600) {
-      // If so, ensure the nav-links are not in the 'active' state
-      navbarlinks.classList.remove("active");
-    }
-  });
+      // Toggle the display of the dropdown content
+      if (window.getComputedStyle(dropdownContent).display === "none") {
+        dropdownContent.style.display = "block";
+      } else {
+        dropdownContent.style.display = "none";
+      }
+    });
+  } else {
+    console.error("Dropdown button not found");
+  }
+
+  // Select the menu bar button
+  var menuBar = document.querySelector(".menubar");
+  // Select the navbar links container
+  var navbarlinks = document.querySelector(".navlinks");
+  var content = document.querySelector(".content");
+
+  // Check if menuBar and navbarlinks exist to avoid null reference errors
+  if (menuBar && navbarlinks) {
+    menuBar.addEventListener("click", function () {
+      // console.info("menu clicked");
+      navbarlinks.classList.toggle("active");
+      if (navbarlinks.classList.contains("active")) {
+        document.body.style.overflowY = "hidden";
+      } else {
+        document.body.style.overflowY = "auto";
+      }
+    });
+
+    window.addEventListener("resize", function () {
+      // Check if the screen width is more than 600px
+      if (window.innerWidth > 600) {
+        // If so, ensure the navbarlinks are not in the 'active' state
+        navbarlinks.classList.remove("active");
+        //Also reset the body overflow
+        document.body.style.overflowY = "auto";
+      }
+    });
+  } else {
+    console.error("Menubar or navbar links not found");
+  }
 });
