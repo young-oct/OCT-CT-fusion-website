7a2c33217e008e2816c1ef2a810c96e6b0d3d1f9
young-oct <>
2023-12-26 01:17:16 -0400

    update: completed publication page

--- HTML/script.js
+++ HTML/script.js
@@ -1,3 +1,65 @@
+// Function to toggle the display of the publication list for a given year
+function toggleYear(id) {
+  var element = document.getElementById(id);
+  var yearDiv = element.previousElementSibling;
+
+  if (element.style.display === "none" || element.style.display === "") {
+    element.style.display = "block";
+    yearDiv.textContent = yearDiv.textContent.replace("+", "-");
+  } else {
+    element.style.display = "none";
+    yearDiv.textContent = yearDiv.textContent.replace("-", "+");
+  }
+}
+
+// Expands all the publication lists
+function expandAll() {
+  var allYears = document.querySelectorAll(".publications");
+  allYears.forEach(function (year) {
+    year.style.display = "block";
+    year.previousElementSibling.textContent = year.previousElementSibling.textContent.replace("+", "-");
+  });
+}
+
+// Contracts all the publication lists
+function contractAll() {
+  var allYears = document.querySelectorAll(".publications");
+  allYears.forEach(function (year) {
+    year.style.display = "none";
+    year.previousElementSibling.textContent = year.previousElementSibling.textContent.replace("-", "+");
+  });
+}
+
+// A function that sets up initial states on page load
+function setupInitialStates() {
+  // Perform setup tasks here, like setting initial display states
+  var allYears = document.querySelectorAll(".publications");
+  // Example: Hide all publication years on initial load
+  allYears.forEach(function (year) {
+    year.style.display = "block"; // This hides all the publication lists
+    year.previousElementSibling.textContent = "-" + year.previousElementSibling.textContent.slice(2);
+  });
+}
+
+function initializeReverseNumbering() {
+  var allPublications = document.querySelectorAll(".publication");
+  var totalPublications = allPublications.length;
+
+  allPublications.forEach(function (pub, index) {
+    // 'pub' is the current publication element
+    // 'index' is the position of 'pub' in the NodeList
+
+    var number = totalPublications - index;
+    var numberingElement = document.createElement("span");
+
+    var titleElement = pub.querySelector("h3");
+    if (titleElement) {
+      titleElement.textContent = number + ". " + titleElement.textContent;
+      numberingElement.style.fontWeight = "bold";
+    }
+  });
+}
+
 document.addEventListener("DOMContentLoaded", function () {
   // Select the dropdown button
   var dropdownBtns = document.querySelectorAll(".subnav_dropbtn");
@@ -46,4 +108,8 @@ document.addEventListener("DOMContentLoaded", function () {
   } else {
     console.error("Menubar or navbar links not found");
   }
+
+  // Initialize functions here
+  setupInitialStates();
+  initializeReverseNumbering();
 });
