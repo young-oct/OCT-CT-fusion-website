89dba3943e3056110b93df3d32ea680007d1c693
young-oct <>
2024-03-23 18:10:22 -0400

    remove dst

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,27 @@
-import { defineConfig } from 'vite';
-import fs from 'fs';
-import path from 'path';
+import { defineConfig } from "vite";
+import fs from "fs";
+import path from "path";
 // Ensure proper plugins are imported, if any are used
 
 export default defineConfig({
-  root: path.resolve(__dirname, 'src'),
-  base: '/',
-  publicDir: path.resolve(__dirname, 'public'),
+  root: path.resolve(__dirname, "src"),
+  // base: '/',
+  publicDir: path.resolve(__dirname, "public"),
 
   // Development server configuration
   server: {
     port: 3000,
-    host: 'localhost',
+    host: "localhost",
     https: false,
   },
 
   // Production build configuration
   build: {
     chunkSizeWarningLimit: 1000,
-    assetsDir: 'assets', // Specify a directory within `dist` for better organization
-    outDir: path.resolve(__dirname, 'dist'), // Ensure the output directory is correctly resolved
+    assetsDir: "assets", // Specify a directory within `dist` for better organization
+    outDir: path.resolve(__dirname, "dist"), // Ensure the output directory is correctly resolved
 
-    minify: 'esbuild', // Use 'esbuild' for faster minification
+    minify: "esbuild", // Use 'esbuild' for faster minification
     cssCodeSplit: true,
     assetsInlineLimit: 4096,
     sourcemap: false,
@@ -31,23 +31,23 @@ export default defineConfig({
       input: {
         ...Object.fromEntries(
           fs
-            .readdirSync(path.resolve(__dirname, 'src'))
-            .filter(file => file.endsWith('.html'))
-            .map(file => [path.basename(file, '.html'), path.resolve(__dirname, 'src', file)])
+            .readdirSync(path.resolve(__dirname, "src"))
+            .filter((file) => file.endsWith(".html"))
+            .map((file) => [path.basename(file, ".html"), path.resolve(__dirname, "src", file)])
         ),
-        main: path.resolve(__dirname, 'src/js/main.js'),
-        script: path.resolve(__dirname, 'src/js/script.js'),
+        main: path.resolve(__dirname, "src/js/main.js"),
+        script: path.resolve(__dirname, "src/js/script.js"),
       },
       output: {
-        entryFileNames: 'assets/[name].[hash].js',
-        chunkFileNames: 'assets/[name].[hash].js',
-        assetFileNames: 'assets/[name].[hash][extname]',
+        entryFileNames: "assets/[name].[hash].js",
+        chunkFileNames: "assets/[name].[hash].js",
+        assetFileNames: "assets/[name].[hash][extname]",
       },
     },
   },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),
+      "@": path.resolve(__dirname, "src"),
       // Add more aliases as needed
     },
   },
