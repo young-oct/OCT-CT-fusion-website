aca8ccbd06a7173e80bd3f5e0055b17619e1b20e
young-oct <>
2024-03-24 13:55:38 -0400

    update base path

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,7 @@ import path from "path";
 
 export default defineConfig({
   root: path.resolve(__dirname, "src"),
-  base: "/OCT-CT-fusion-website/",
-  // base: '/',
+  // base: "/OCT-CT-fusion-website/",
   publicDir: path.resolve(__dirname, "public"),
 
   // Development server configuration
